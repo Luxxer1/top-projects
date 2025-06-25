00599f86486d16ce532a8746916e7d6ec5424c54
Lucas Bastos <lfsouzabastos@gmail.com>
2025-06-25 12:07:57 -0300

    choice buttons working

--- 3-rock-paper-scissors/script.js
+++ 3-rock-paper-scissors/script.js
@@ -1,33 +1,51 @@
-const choices = document.querySelectorAll("button");
-const score = document.querySelector("#score");
-const result = document.querySelector("#result");
+const humanChoice = document
+  .getElementById("human")
+  .getElementsByClassName("choice")[0];
+
+const computerChoice = document
+  .getElementById("computer")
+  .getElementsByClassName("choice")[0];
+
+const choices = document.querySelectorAll(".choices");
+const start = document.querySelector("#start");
+start.addEventListener("click", startGame);
 
 let computerScore = 0,
   humanScore = 0;
+let R = "rock",
+  P = "paper",
+  S = "scissors";
 let gameOver = false;
 
 choices.forEach((choice) => {
-  choice.addEventListener("click", "");
+  choice.addEventListener("click", getHumanChoice);
 });
 
 function getComputerChoice() {
   randomNumber = Math.round(Math.random() * (3 - 1) + 1);
   switch (randomNumber) {
     case 1:
+      computerChoice.innerHTML = `<img src="/assets/${R}.png" alt="${R}">`;
       return R;
 
     case 2:
-      return S;
+      computerChoice.innerHTML = `<img src="/assets/${P}.png" alt="${P}">`;
+      return P;
 
     case 3:
-      return P;
+      computerChoice.innerHTML = `<img src="/assets/${S}.png" alt="${S}">`;
+      return S;
 
     default:
       return console.error("Error, math randomly went wrong");
   }
 }
 
+function startGame() {}
+
 function getHumanChoice(e) {
+  humanChoice.innerHTML = `<img src="/assets/${e.target.id}.png" alt="${e.target.id}">`;
+  getComputerChoice();
   return e.target.id;
 }
 
