44393db02ff63df47642a5e3ebf8fbc6f355a2aa
Lucas Bastos <lfsouzabastos@gmail.com>
2025-08-06 16:19:03 -0300

    create function hover changing color and opacity

--- 4-etch-a-sketch/script.js
+++ 4-etch-a-sketch/script.js
@@ -3,9 +3,33 @@ let container = document.getElementById("container");
 const createGrid = (function () {
   return function (number) {
     for (let i = 1; i <= number; i++) {
-      container.innerHTML += `Number ${i} `;
+      const square = document.createElement("div");
+
+      square.setAttribute("class", "square");
+      square.addEventListener("mouseover", changeSquareBackground.bind(null, square));
+
+      container.appendChild(square);
     }
   };
 })();
 
-createGrid(25);
+const changeSquareBackground = (square) => {
+  const red = getRandomRgbValue();
+  const blue = getRandomRgbValue();
+  const green = getRandomRgbValue();
+  const opacity = 0.1;
+
+  square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  square.style.opacity != "" ?
+    square.style.opacity = parseFloat(square.style.opacity) + opacity :
+    square.style.opacity = opacity;
+}
+
+const getRandomRgbValue = () => {
+  const min = 0;
+  const max = 255;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+createGrid(200);
