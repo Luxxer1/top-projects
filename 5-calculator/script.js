3c67ba8bdbaebec2918a1bb914c54b3250223cb7
Lucas Bastos <lfsouzabastos@gmail.com>
2025-09-24 19:02:08 -0300

    implement refresh display - clear

--- 5-calculator/script.js
+++ 5-calculator/script.js
@@ -2,6 +2,12 @@ import { add, subtract, multiply, divide } from "./calculator.js";
 
 const result = document.getElementById("result");
 const display = document.getElementById("display");
+const buttons = document.querySelectorAll("#calculator > button");
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    refreshDisplay(button);
+  })
+);
 
 const operate = (operate, n1, n2) => {
   switch (operate) {
@@ -18,3 +24,22 @@ const operate = (operate, n1, n2) => {
       return divide(n1, n2);
   }
 };
+
+function refreshDisplay(button) {
+  switch (button.className) {
+    case "number":
+      break;
+
+    case "operator":
+      break;
+
+    case "evaluate":
+      break;
+
+    case "clear":
+      result.innerHTML += "";
+      display.innerHTML += "";
+      break;
+  }
+  // display.innerHTML += button.textContent;
+}
