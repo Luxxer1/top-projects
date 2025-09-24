c78bdeff750e4064f3e28613bbbbc7db860adc81
Lucas Bastos <lfsouzabastos@gmail.com>
2025-09-24 18:39:08 -0300

    create operate function

--- 5-calculator/script.js
+++ 5-calculator/script.js
@@ -2,3 +2,19 @@ import { add, subtract, multiply, divide } from "./calculator.js";
 
 const result = document.getElementById("result");
 const display = document.getElementById("display");
+
+const operate = (operate, n1, n2) => {
+  switch (operate) {
+    case "+":
+      return add(n1, n2);
+
+    case "-":
+      return subtract(n1, n2);
+
+    case "*":
+      return multiply(n1, n2);
+
+    case "/":
+      return divide(n1, n2);
+  }
+};
