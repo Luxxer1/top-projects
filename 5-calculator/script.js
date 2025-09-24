3e678f6093b42cebfe7070b0d7af3fb301ebed64
Lucas Bastos <lfsouzabastos@gmail.com>
2025-09-24 20:08:33 -0300

    implemented function refreshDisplay

--- 5-calculator/script.js
+++ 5-calculator/script.js
@@ -1,5 +1,7 @@
 import { add, subtract, multiply, divide } from "./calculator.js";
 
+let operator, n1, n2, resultCalculated;
+
 const result = document.getElementById("result");
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("#calculator > button");
@@ -8,6 +10,8 @@ buttons.forEach((button) =>
     refreshDisplay(button);
   })
 );
+
+const clearDisplay = () => (display.innerHTML = "");
 
 const operate = (operate, n1, n2) => {
   switch (operate) {
@@ -28,19 +32,60 @@ const operate = (operate, n1, n2) => {
 function refreshDisplay(button) {
   switch (button.className) {
     case "number":
-      display.innerHTML += button.innerText;
+      display.innerText += button.innerText;
       break;
 
     case "operator":
+      if (!operator) {
+        n1 = +display.innerText;
+        operator = button.innerText;
+        result.innerText = `${n1} ${operator}`;
+        clearDisplay();
+      } else {
+        if (display.innerText == "") {
+          operator = button.innerText;
+          result.innerText = `${n1} ${operator}`;
+          clearDisplay();
+        } else {
+          if (resultCalculated) {
+            resultCalculated = undefined;
+            operator = button.innerText;
+            n1 = +display.innerText;
+            result.innerText = `${n1} ${operator}`;
+            clearDisplay();
+          } else {
+            n2 = +display.innerText;
+            let [num1, op] = result.innerText.split(" ");
+            n1 = operate(op, +num1, n2);
+            operator = button.innerText;
+            result.innerText = `${n1} ${operator}`;
+            clearDisplay();
+          }
+        }
+      }
+
       break;
 
     case "evaluate":
+      if (result.innerText == "" || display.innerText == "") {
+        display.innerText = "Error, something is missing";
+      } else {
+        let [num1, op] = result.innerText.split(" ");
+        n2 = +display.innerText;
+        n1 = operate(op, +num1, n2);
+        resultCalculated = n1;
+        result.innerText = n1;
+        clearDisplay();
+      }
       break;
 
     case "clear":
       result.innerHTML = "";
-      display.innerHTML = "";
+      clearDisplay();
+      n1 = undefined;
+      n2 = undefined;
+      operator = undefined;
+      resultCalculated = undefined;
       break;
   }
-  // display.innerHTML += button.textContent;
 }
