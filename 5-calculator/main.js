edbdf86e4fb1910ef47ba8f1a111fa4a2b244f59
Lucas Bastos <lfsouzabastos@gmail.com>
2025-10-07 10:13:40 -0300

    create calculator object, identifyButton and appendDigit

--- 5-calculator/main.js
+++ 5-calculator/main.js
@@ -1,1 +1,34 @@
-document.querySelectorAll("#calculator > button").forEach((button) => {});
+const evaluationDisplay = document.getElementById("evaluationDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+///////////////////////////////////////////////////////////////////////
+
+const calculator = {
+  n1: undefined,
+  n2: undefined,
+  operator: undefined,
+  result: undefined,
+};
+
+const appendDigit = (digit) => {
+  evaluationDisplay.innerText += digit;
+};
+
+const identifyButton = (button) => {
+  switch (button.className) {
+    case "number":
+      appendDigit(button.innerText);
+      break;
+
+    default:
+      break;
+  }
+};
+
+///////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll("#calculator > button").forEach((button) => {
+  button.addEventListener("click", () => {
+    identifyButton(button);
+  });
+});
